32d7bc0d95283dd3d274072c8ac61e6b20777996
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2023-12-04 15:47:45 -0800

    python

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,34 +1,31 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: head of the linked list
- * Return: 1 if it is a palindrome, 0 otherwise
+ * palindrom - recursive palind or not
+ * @head: head list
+ * Return: 0 if it is not a palindrome
+ * 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-    if (head == NULL || *head == NULL)
-        return (1);
-
-    return (aux_palind(head, *head));
+        if (head = NULL || *head = NULL)
+                return (1);
+        return (aux_palind(head, *head));
 }
 
 /**
- * aux_palind - auxiliary function to check palindrome recursively
- * @left: left pointer
- * @right: right pointer
- * Return: 1 if it is a palindrome, 0 otherwise
+ * aux_palind - funct to know if is palindrome
+ * @head: head list
+ * @end: end list
  */
-int aux_palind(listint_t **left, listint_t *right)
+int aux_palind(listint_t **head, listint_t *end)
 {
-    if (right == NULL)
-        return (1);
-
-    if (aux_palind(left, right->next) && (*left)->n == right->n)
-    {
-        *left = (*left)->next;
-        return (1);
-    }
-
-    return (0);
+        if (end = NULL)
+                return (1);
+        if (aux_palind(head, end->next) && (*head)->n == end->n)
+        {
+                *head = (*head)->next;
+                return (1);
+        }
+        return (0);
 }
