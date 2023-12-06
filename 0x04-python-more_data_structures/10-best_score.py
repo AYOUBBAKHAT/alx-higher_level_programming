b124564bb5b3b4408e10c85784942fefc69219ba
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2023-12-06 15:20:43 -0800

    python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return None:
-            return None
+        return None
     maxval = 0
     maxkey = None
     for k, v in a_dictionary.items():
