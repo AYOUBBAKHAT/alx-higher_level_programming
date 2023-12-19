c31f091f35b7c7aa668d97006bee8a190e1e98d3
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2023-12-19 15:35:47 -0800

    python

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of a side of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Property for the length of a side of this square.
+
+         Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.size = value
