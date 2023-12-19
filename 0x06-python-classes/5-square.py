a391c3fa4951649df95187196572cdd0423441f5
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2023-12-19 15:53:46 -0800

    python

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -42,6 +42,6 @@ class Square:
     def my_print(self):
         """Prints this square."""
         for i in range(self.size):
-            for i in range(self.size):
+            for j in range(self.size):
                 print("#", end="\n" if j is self.size - 1 and i != j else "")
         print()
