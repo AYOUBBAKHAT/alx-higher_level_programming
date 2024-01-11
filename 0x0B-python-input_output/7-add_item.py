2f6f494dd805b24949453827008b5302d39b4f98
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-01-11 14:44:04 -0800

    py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
 '''task 7 module'''
 
-
 import sys
+from json import JSONDecodeError
+from pathlib import Path
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arglist = list(sys.argv[1:])
+def main():
+    arglist = list(sys.argv[1:])
 
-try:
-    old_data = load_from_json_file('add_item.json')
-except Exception:
-    old_data = []
+    try:
+        old_data = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        old_data = []
+    except JSONDecodeError:
+        print('Error: Unable to decode JSON from add_item.json')
+        sys.exit(1)
+    
+    old_data.extend(arglist)
 
-old_data.extend(arglist)
-save_to_json_file(old_data, 'add_item.json')
+    try:
+        save_to_json_file(old_data, 'add_item.json')
+        print('Items added successfully.')
+    except Exception as e:
+        print(f'Error: {e}')
+
+if __name__ == "__main__":
+    main()
+
