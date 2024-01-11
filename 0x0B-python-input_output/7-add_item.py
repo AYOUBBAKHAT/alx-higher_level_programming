62f6aeca4ee8ba6a86bcaa7c95bb3fd8480b0e4c
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-01-11 14:52:02 -0800

    py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-'''task 7 module'''
+"""Load, add, save"""
 
 
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from sys import argv
+from os.path import exists
 
-arglist = list(sys.argv[1:])
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-try:
-    old_data = load_from_json_file('add_item.json')
-except Exception:
-    old_data = []
+namefile = "add_item.json"
+argc = len(argv)
 
-old_data.extend(arglist)
-save_to_json_file(old_data, 'add_item.json')
+new_list = []
+
+if exists(namefile):
+    new_list = load_from_json_file(namefile)
+
+if (argc == 1):
+    save_to_json_file([], namefile)
+else:
+    for i in range(1, argc):
+        new_list.append(argv[i])
+    save_to_json_file(new_list, namefile)
