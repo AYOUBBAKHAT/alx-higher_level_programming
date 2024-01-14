378bb35538d4ec0c0065b110f2e2e6905cbeec1a
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-01-14 03:50:02 -0800

    py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module class LockedClass
+"""
+
+
+class LockedClass:
+    """ Locked Class
+    you can not add any other attributs
+    """
+
+    __slots__ = ['first_name']
