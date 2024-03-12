5618c737d1d995a7af3ae43b07633d062401100b
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-03-12 02:11:40 +0000

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-strings.forEach(string => {
-	console.log(string);
-});
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (const item of array) {
+	console.log(item);
+}
