ab0d971a39486a72077dcf9a98010132c7ec4f09
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-03-14 00:11:05 +0000

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,20 @@
 #!/usr/bin/node
 const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-const arrayLength = myLines.length;
+
+// Existing lines
+const existingLines = myLines.length;
 let i = 0;
-while (i < arrayLength) {
+while (i < existingLines) {
 	console.log(myLines[i]);
 	i++;
 }
+
+// Additional lines if necessary
+const additionalLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const missingLines = additionalLines.length - existingLines;
+i = 0;
+while (i < missingLines) {
+	console.log(additionalLines[i]);
+	i++;
+}
+
