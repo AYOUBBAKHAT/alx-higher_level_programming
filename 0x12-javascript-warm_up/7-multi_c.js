985b4a379614221e56474a9f056bc15fc3dfcc33
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-03-14 00:15:54 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-	console.log('Missing number of occurrences');
+const x = process.argv[2];
+if (isNaN(x) || x === undefined) {
+	console.log("Missing number of occurrences");
 } else {
-	const x = Number(process.argv[2]);
-	let i = 0;
-	while (i < x) {
-		console.log('C is fun');
-		i++;
+	const num = parseInt(x);
+	for (let i = 0; i < num; i++) {
+		console.log("C is fun");
 	}
 }
