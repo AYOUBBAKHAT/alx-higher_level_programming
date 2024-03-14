a483e38039b6e4f45eddcfe742dbd660649f9e29
AYOUBBAKHAT <bakhat.ayoubbusinnes@gmail.com>
2024-03-14 00:37:36 +0000

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i = 0; i < lines.length; i++) {
-	console.log(lines[i]);
+
+// This line specifies the path to the interpreter for the script.
+
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	// This loop iterates over each element in the array,
+	// ['C is fun', 'Python is cool', 'JavaScript is amazing'].
+	// The "line" variable is assigned the value of each element in,
+	// the array during each iteration.
+	console.log(line);
+	// This line outputs the value of the "line" variable to the console.
 }
